a5817128b559caaff761dba962ce2935683e25a7
NiklasHj <niklas.hjelm@ga.ntig.se>
2020-11-17 10:40:35 +0100

    Added form with post

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const staticDir = __dirname + '\\client\\'
+
+app.use(express.static(staticDir))
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.post('/', function (req, res) {
+    res.send(req.body.message)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
